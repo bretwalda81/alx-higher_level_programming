b54a8d645de4d5ce9763a7e85a77a7ad8b8fc3e9
Akinbowale <akin.akinyemi81@gmail.com>
2022-09-22 13:29:48 -0700

    print alphabet in uppercase

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,8 @@
 if __name__ == '__main__':
     import hidden_4
 
-    for i in dir(hidden_4):
-        if i[0] != '_' and i[1] != '_':
-            print("{}". format(i))
-y
+    list = dir(hidden_4)
+    num = len(dir(hidden_4))
+    for i in range(num):
+        if list[i][:2] != "__":
+            print(list[i])
