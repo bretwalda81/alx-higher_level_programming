ced0cf32e083b6575487b6231f5aace94883d3b0
Akinbowale <akin.akinyemi81@gmail.com>
2022-09-22 13:48:56 -0700

    handle basic arithmetic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    from calculator_1 import add, sub, div, mul
+    from calculator_1 import add, sub, mul, div
 
     def calc(func, a, b, sign):
         c = func(a, b)
