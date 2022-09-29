e0c6253915f70dac72225c0cd4fc40324cd62119
Akinbowale <akin.akinyemi81@gmail.com>
2022-09-28 21:19:19 -0700

    return defoned values

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,4 +3,4 @@ def square_matrix_simple(matrix=[]):
     newlist = []
     for eachlist in matrix:
         newlist.append(list(map(lambda a: a * a, eachlist)))
-    return
+    return newlist
--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -4,4 +4,4 @@ def common_elements(set_1, set_2):
     for i in set_1:
         if i in set_2:
             common_list.append(i)
-    return
+    return common_list
